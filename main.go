46f98fc4471ae37676af01c1269ab32bd5ed1bd5
GTeasera <87891550+GTeasera@users.noreply.github.com>
2022-06-19 00:47:15 +0300

    0.0.2

--- main.go
+++ main.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+var a, b, c int
+
 func main() {
-	fmt.Println("I will be GO developer")
+	var message = "Go to Go"
+	const message1 string = "ffds"
+
+	fmt.Println(message1)
+	fmt.Println(message)
+	fmt.Println("I will be Go developer")
+
+	// data type - https://metanit.com/go/tutorial/2.3.php
+
+	/*
+		a, b, c := 1, 2, 3
+		fmt.Println(a, b, c)
+	*/
+
+	a, _, c = c, a, 8
+	fmt.Println(a, b, c)
+
+	a, b, c = 6, 7, 8
+	print()
+}
+
+func print() {
+	fmt.Println(a, b, c)
 }
